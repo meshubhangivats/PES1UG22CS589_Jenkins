bcb73926f1b499ba00f6141f22e9e49eca98854b
meshubhangivats <143866108+meshubhangivats@users.noreply.github.com>
2025-03-14 19:10:09 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main() {
     // Loop from 1 to 5
     for (int i = 1; i <= 5; i++) {
-        cout << i << endl;// Print each number followed by a new line
+        cout << i << endl// Print each number followed by a new line
     }
     return 0;
 }
